d49965ce0383fc9ba68dda6df63b264d08a7e995
Diana Perez <dianaperezc10@gmail.com>
2024-11-01 22:47:27 -0500

    Index Categories

--- SupermarketWEB/Data/SupermarketContext.cs
+++ SupermarketWEB/Data/SupermarketContext.cs
@@ -9,7 +9,8 @@ namespace SupermarketWEB.Data
         { 
         }
 
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Category> Categories { get; set; }
-    }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Category> Categories { get; set; }
+
+	}
 }
--- SupermarketWEB/Models/Category.cs
+++ SupermarketWEB/Models/Category.cs
@@ -1,7 +1,10 @@
-﻿namespace SupermarketWEB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketWEB.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; } //llave primaria 
         public string Name { get; set; }   
         public string? Description { get; set; }
