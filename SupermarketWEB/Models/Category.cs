e1205d369e74687a6cf1e02bf415301b8f8d39c4
Diana Perez <dianaperezc10@gmail.com>
2024-11-01 22:57:10 -0500

    Delete Categories

--- SupermarketWEB/Models/Category.cs
+++ SupermarketWEB/Models/Category.cs
@@ -8,6 +8,6 @@ namespace SupermarketWEB.Models
         public int Id { get; set; } //llave primaria 
         public string Name { get; set; }   
         public string? Description { get; set; }
-        public ICollection <Product> Products { get; set; }
+        public ICollection <Product>? Products { get; set; }
     }
 }
