938a1ed05c88e115c3d067477253a6698e2f6e85
Diana Perez <dianaperezc10@gmail.com>
2024-11-01 22:00:19 -0500

    (message) Modelos y configuracion del DbContext

---
+++ SupermarketWEB/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class Category
+    {
+        public int Id { get; set; } //llave primaria 
+        public string Name { get; set; }   
+        public string? Description { get; set; }
+        public ICollection <Product> Products { get; set; }
+    }
+}
--- SupermarketWEB/Program.cs
+++ SupermarketWEB/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+
 namespace SupermarketWEB
 {
     public class Program
@@ -8,6 +11,11 @@ namespace SupermarketWEB
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+
+            //Agrgando el contexto SupermarketContext 
+            builder.Services.AddDbContext<SupermarketContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SupermarketDB"))
+            );
 
             var app = builder.Build();
 
