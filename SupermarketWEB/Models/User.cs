43dd40bdf45b53e9a693033915735f31bb07ed66
Diana Perez <dianaperezc10@gmail.com>
2024-11-04 15:39:52 -0500

    creacion de modelo user

---
+++ SupermarketWEB/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Autenticacion.Model
+{
+    public class User
+    {
+        public int ID { get; set; } 
+
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; } 
+
+        [Required]
+        public byte[] PasswordHash { get; set; } 
+    }
+}
