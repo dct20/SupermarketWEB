90222f9817457522d95693ac6350202c59675a1b
Diana Perez <dianaperezc10@gmail.com>
2024-11-04 14:31:30 -0500

    creacion del edit de paymode

--- SupermarketWEB/Pages/PayModels/Create.cshtml.cs
+++ SupermarketWEB/Pages/PayModels/Create.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.PayModels
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly SupermarketContext _context;
+
+        public CreateModel(SupermarketContext context)
         {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public PayMode PayMode { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.PayModes == null || PayMode == null)
+            {
+                return Page();
+            }
+
+            _context.PayModes.Add(PayMode);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
